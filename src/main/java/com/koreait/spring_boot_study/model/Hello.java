99d6af9e9ce017ab7cb7c90092e43035f1c98faf
박정빈 <jeongbin@bagjeongbin-ui-MacBookPro.local>
2025-11-12 21:44:39 +0900

    new: Controller

---
+++ src/main/java/com/koreait/spring_boot_study/model/Hello.java
@@ -0,0 +1,12 @@
+package com.koreait.spring_boot_study.model;
+
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class Hello {
+    private String hello1;
+    private String hello2;
+}
