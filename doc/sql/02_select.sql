e68dc119546fbd38a5866afda196d430ec680aea
박정빈 <jeongbin@bagjeongbin-ui-MacBookPro.local>
2025-11-21 21:48:57 +0900

    add: update, delete, sub

--- doc/sql/02_select.sql
+++ doc/sql/02_select.sql
@@ -112,9 +112,13 @@ from
 	product
 where
 	product_name like "마%"; # "마"로 시작하는 이름 
-# 마% : "마"로 시작하는 이름
+# 마% : "마"로 시작하는 이름 -> fast(unique혹은 pk가 걸려있어야 한다)
 # %마 : "마"로 끝나는 이름
 # %마% : "마"가 포함된 이름
+# 박%목% : "박"으로 시작하고, "목"을 포함하는 이름
+# 김__: 김으로 시작하고, 3글자 -> fast(unique혹은 pk가 걸려있어야 한다)
+# 김_ : 김으로 시작하고, 2글자
+# _화_: 중간이 "화"인 3글자
 
 
 # 실습1) 북으로 끝나는 상품을 조회해 주세요
@@ -149,7 +153,8 @@ from
 order by 
 	product_price desc
 limit 4 offset 4; 
-# order by에 들어간 컬럼은 select에 있는것이 표준
+# order by에 들어간 컬럼은 select에 있는것이 표준 -> mysql은 허용
+# 다만, distinct를 걸면, 반드시 있어야 한다.
 # select에서 product_name만 입력하면 가격순 5~8등을 보장하지 못함
 
 
@@ -160,3 +165,67 @@ select
     product_price * 1.1 as `부가세포함`# 부가세를 포함한 가격
 from
 	product;
+
+# 집계함수
+select
+	count(*) # product에 등록된 상품의 갯수(row의 수/ 가로줄)
+    # count(*) -> null도 카운팅한다
+    # count(특정컬럼) -> 해당 컬럼의 값이 null이면 카운팅 안한다.
+from
+	product;
+
+select
+	avg(Product_price) as `상품가격 평균` # 평균
+    # max(product_price) -> 상품가격 중 최대 가격
+    # min(prodcut_price) -> 상품가격 중 최소 가격
+from
+	product;
+
+
+# 그룹화 & case문법 - group by
+# 저가, 중가, 고가를 임의로 나누고, 각 카테고리에 상품들이 몇개씩 있는가?
+select
+	# case문(조건문)
+       case # if문 시작
+		when Product_price <= 50000 then '저가'
+        when Product_price <= 100000 then '중가'
+        else '고가'
+	end as `price_range`, # if문 종료
+    count(*) as `counting`
+from
+	product
+group by
+	price_range # price_range의 결과가 같은것들을 하나로 묶겠다.
+having
+	count(*) >= 3; # 각 그룹의 count결과가 3이상인 것들만 -> 그룹조건
+
+/*
+select 전체 실행 순서
+: row(가로줄)를 먼저 제거하는게 우선순위가 높다.
+select, where, from, group by, having, order by
+1. from : 어떤 테이블에서 데이터를 가져올지 결정
+2. where : 가져온 데이터 중 조건을 만족하는 row만 남김
+3. group by : 행을 그룹으로 묶음
+4. having : 그룹 조건 -> 조건을 만족하는 그룹만 남김
+5. select : 어떤 컬럼(세로줄)을 출력할건지 결정
+6. order by: 출력 순서 조정
+7. limit : 몇 개까지 출력할지 제한
+*/
+
+# 실습) case문과 group by를 사용하여 저가, 중가, 고가별 평균가격을 구해주세요.
+select
+	# product_name -> 그룹화 해버린 상태에서 한칸에 여러개의 이름을 표현하세요 -> 불가능
+    # group by에 있는 컬럼이거나, 집계함수는 select에 선언할 수 있다.
+    CASE 
+        WHEN product_price <= 50000 THEN '저가'
+        WHEN product_price <= 100000 THEN '중가'
+        ELSE '고가'
+    END AS `price_range`, # price_range 컬럼에 들어갈 값은 저가, 주가, 고가 중 하나
+    avg(product_price) as `평균가` # 평균가라는 컬럼
+from
+	product
+group by 
+	price_range;
+    
+
+
